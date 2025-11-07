ed33dde4263ec5397f40cd0b54a71fb08c7fce64
Pedro <pedro.dnunes1@gmail.com>
2025-11-07 18:43:02 -0300

    Fresh start

---
+++ migrations/20250925202859-create-link.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Links', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      originalUrl: {
+        type: Sequelize.STRING
+      },
+      shortCode: {
+        type: Sequelize.STRING
+      },
+      clicks: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Links');
+  }
+};
